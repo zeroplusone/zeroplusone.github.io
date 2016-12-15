33db38444256dbc0a6c5aa7e9d62c5eed6f74bb5
zeroplusone <a711186@gmail.com>
2016-12-16 02:43:19 +0800

    add all data in resume.json into index.html

--- js/resume.js
+++ js/resume.js
@@ -28,21 +28,62 @@ $.getJSON("data/resume.json", function(data) {
     /* experience part*/
     var exp = data.work;
     str="<h1>Experience</h1>";
+    str+="<ul>"
+    for(var i =0;i<exp.length;++i){
+        str+="<li>";
+        str+=exp[i].startDate+" ~ "+exp[i].endDate+": <b>"+exp[i].company+" "+exp[i].position+"</b>";
+        str+="<ul>";
+        str+="<li>"+exp[i].summary+"</li>";
+        str+="</ul>";
+        str+="</li>";
+    }
+    str+="</ul>";
     document.getElementById("experience").innerHTML = str;
 
     /* publication part*/
-    var pub = data.work;
+    var pub = data.publications;
     str="<h1>Publication</h1>";
+    str+="<ul>";
+    for(var i =0;i<pub.length;++i){
+        str+="<li>";
+        str+="<a href="+pub[i].website+"><b>"+pub[i].name+"</b></a>";
+        str+="<ul>";
+        str+="<li> publisher: "+pub[i].publisher+"</li>";
+        str+="<li> release date: "+pub[i].releaseDate+"</li>";
+        str+="</ul>";
+        str+="</li>";
+    }
+    str+="</ul>";
     document.getElementById("publication").innerHTML = str;
 
     /* awards part*/
-    var awards = data.work;
+    var awards = data.awards;
     str="<h1>Awards</h1>";
+    str+="<ul>";
+    for(var i =0;i<awards.length;++i){
+        str+="<li>";
+        str+=awards[i].date+" "+awards[i].title;
+        str+="<ul>";
+        str+="<li> awarder: "+awards[i].awarder+"</li>";
+        str+="</ul>";
+        str+="</li>";
+    }
+    str+="</ul>";
     document.getElementById("awards").innerHTML = str;
 
     /* Projects part*/
-    var proj = data.work;
+    var proj = data.skills;
     str="<h1>Projects</h1>";
+    str+="<ul>";
+    for(var i =0;i<proj.length;++i){
+        str+="<li>";
+        str+=proj[i].name;
+        str+="<ul>";
+        str+="<li> prject list: coming soon... </li>";
+        str+="</ul>";
+        str+="</li>";
+    }
+    str+="</ul>";
     document.getElementById("projects").innerHTML = str;
 
 });
